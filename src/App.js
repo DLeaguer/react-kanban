1da521cba56808ba6ed1e6b80b08454a672669b2
DLeaguer <immostlikely@hotmail.com>
2018-10-08 12:53:29 -1000

    cards function for render, mount database from db.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getItemsFromFakeXHR, addItemToFakeXHR, deleteItemByIdFromFakeXHR } from './db/db';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: [{
-        id: 1,
-        description: 'Make Better Styles.',
-        priority: 'Medium',
-        by: 'Jon',
-        to: 'Renee'
-      },
-      {
-        id: 2,
-        description: 'Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles.',
-        priority: 'Medium',
-        by: 'Jon',
-        to: 'Renee'
-      }]
+      items: []
     }
+  }
+
+  componentDidMount() {
+    getItemsFromFakeXHR()
+      .then( items => {
+        this.setState( {items} )
+      })
   }
 
   render() {
@@ -41,35 +36,37 @@ class App extends Component {
         
         {/* CARDS */}
         <div className='items'>
-
-          {this.state.items.map( item => 
-          <div className='card'>
-
-            <div className='infostrong'> 
-              {item.description} 
-            </div>
-            
-            <div className='info'> 
-              Priority: {item.priority} 
-            </div> 
-            
-            <div className='info'> 
-              Assigned by: {item.by} 
-            </div>
-            
-            <div className='emp'>
-              <div className='edit'> Edit </div>
-              <div className='edit'> Delete </div>
-              <div className='worker'> {item.to} </div> 
-            </div>
-            
-          </div>)}  
-          
+          <Cards items={this.state.items}/>
         </div>
         
       </div>
     )
   }
+}
+
+function Cards(props) {
+  return props.items.map( item => 
+  <div className='card'>
+
+    <div className='infostrong'> 
+      {item.description} 
+    </div>
+    
+    <div className='info'> 
+      Priority: {item.priority} 
+    </div> 
+    
+    <div className='info'> 
+      Assigned by: {item.by} 
+    </div>
+    
+    <div className='emp'>
+      <div className='edit'> Edit </div>
+      <div className='edit'> Delete </div>
+      <div className='worker'> {item.to} </div> 
+    </div>
+    
+  </div>)
 }
 
 export default App;
