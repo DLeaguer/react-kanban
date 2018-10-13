acb84c1a691ffc7d5ba33145ee0d4c296fbbc5fe
DLeaguer <immostlikely@hotmail.com>
2018-10-12 22:06:37 -1000

    migrate/seed, proxy server, route, add form

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.EXPRESS_CONTAINER_PORT || 9999
+const path = require('path')
+const Tasks = require('./db/models/tasks.js');
+
+
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('/', (req, res) => {
+  // res.sendFile('../public/index.html')
+  res.send('Hello')
+})
+
+app.get('/tasks', (req, res) => {
+  // res.json({
+  //   items: [{
+  //       id: 1,
+  //       name: 'A Large Healing Potion',
+  //       weight: 0.1,
+  //       type: 'consumable'
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'Wirts Leg',
+  //       weight: 10,
+  //       type: 'weapon'
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'Dreamwalker Spaulders',
+  //       weight: 2,
+  //       type: 'armor'
+  //     }
+  //   ]
+  // })
+  Tasks
+  .fetchAll()
+  .then( items => {
+    res.json(items.serialize())
+  }) 
+  .catch( err => {
+    console.log('error', err)
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`)
+})
+
